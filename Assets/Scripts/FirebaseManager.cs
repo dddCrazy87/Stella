55917849b8845095b68ab21368106d9198b8fc12
劉丞恩 <114967562+Joanne0119@users.noreply.github.com>
2024-05-11 16:19:09 +0800

    add Login and Logout function

--- Assets/Scripts/FirebaseManager.cs
+++ Assets/Scripts/FirebaseManager.cs
@@ -6,16 +6,35 @@ public class FirebaseManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public Firebase.Auth.FirebaseAuth auth;
-    public Firebase.Auth.FirebaseAuth user;
+    public Firebase.Auth.FirebaseUser user;
     void Start()
     {
         auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        auth.StateChanged += AuthStateChanged; 
+        //訂閱函式: 當Auth切換狀態就會觸發AuthStateChanged
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void LogIn(string email, string password){
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+            if(task.IsFaulted){
+                print("faulted!");
+                print(task.Exception.InnerException.Message);
+                return;
+            }
+            if(task.IsCompletedSuccessfully){
+                print("Login!");
+            }
+        });
+    }
+
+    public void LogOut(){
+        auth.SignOut();
     }
 
     public void Register(string email, string password){
@@ -35,5 +54,22 @@ public class FirebaseManager : MonoBehaviour
                 print("register!");
             }
         });
+    }
+
+    void AuthStateChanged(object sender, System.EventArgs eventArgs){
+        //檢查現在的使用者是不是user
+        //不等於 表示角色有切換AuthStateChanged（登入到登出、登出到登入）
+        if(auth.CurrentUser != user){
+            user = auth.CurrentUser; //更新使用者
+            if(user != null){
+                print($"Login! user email: {user.Email}");
+            }
+        } 
+        
+    }
+
+    //  當物件被刪除時，解除訂閱
+    void OnDestroy() {
+        auth.StateChanged -= AuthStateChanged; 
     }
 }
--- Assets/Scripts/LoginScenes.cs
+++ Assets/Scripts/LoginScenes.cs
@@ -12,9 +12,17 @@ public class LoginScenes : MonoBehaviour
     InputField inputEmail;
     [SerializeField]
     InputField inputPassword;
+    [SerializeField]
+    GameObject panelLogin;
+    [SerializeField]
+    GameObject panelInfo;
+
+    [SerializeField]
+    Text textEmail;
+
     void Start()
     {
-        
+        firebaseManager.auth.StateChanged += AuthStateChanged; 
     }
 
     // Update is called once per frame
@@ -23,7 +31,34 @@ public class LoginScenes : MonoBehaviour
         
     }
 
+    public void LogIn(){
+        firebaseManager.LogIn(inputEmail.text, inputPassword.text);
+    }
+
+    public void LogOut(){
+        firebaseManager.LogOut();
+    }
+
     public void Register(){
         firebaseManager.Register(inputEmail.text, inputPassword.text);
     }
+
+    void AuthStateChanged(object sender, System.EventArgs eventArgs){
+        if(firebaseManager.user == null){
+            textEmail.text = "";
+            panelLogin .SetActive(true);
+            panelInfo .SetActive(false);
+
+        } else{
+            textEmail.text = firebaseManager.user.Email;
+            panelLogin .SetActive(false);
+            panelInfo .SetActive(true);
+        }
+        
+    }
+
+    void OnDestroy() {
+        firebaseManager.auth.StateChanged -= AuthStateChanged; 
+    }
+
 }
