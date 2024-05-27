14f7907029c8fc527ae032d6078041abbf2094a4
dddCrazy87 <n126970@gmail.com>
2024-05-27 23:42:41 +0800

    update

---
+++ Assets/Scripts/Main/BookUI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookUI : MonoBehaviour
+{
+    [SerializeField] private GameObject bookCanvas;
+
+    private void Start() {
+        bookCanvas.SetActive(false);
+    }
+
+    public void openBook() {
+        bookCanvas.SetActive(true);
+    }
+
+    public void closeBook() {
+        bookCanvas.SetActive(false);
+    }
+}
