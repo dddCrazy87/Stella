28e85289cd50117c71bd23b8e10042959d711616
dddCrazy87 <n126970@gmail.com>
2024-05-27 23:55:25 +0800

    update

--- Assets/Scripts/UserTest/UserTest.cs
+++ Assets/Scripts/UserTest/UserTest.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class UserTest : MonoBehaviour
 {
     [SerializeField] private MindMapProjs mindMapProjs;
-    [SerializeField] private UserInfo userInfo;
-    void Start()
-    {
-        
+    [SerializeField] private ProjsUIManager projsUIManager;
+    private void Start() {
+        mindMapProjs.mindMapProjs = new List<MindMapNode> { 
+            new("早餐", "寫下一個主題吧！", 0),
+            new("午餐", "寫下一個主題吧！寫下一個主題吧！", 0),
+            new("晚餐", "寫下一個主題吧！寫下一個主題吧！寫下一個主題吧！", 0)
+        };
+        projsUIManager.setProjsUI();
     }
+
+    //(string nodeText, string nodeQusetion, int nodeLevel, List<MindMapNode> childrenNode)
+    //(string nodeText, string nodeQusetion, int nodeLevel)
 }
